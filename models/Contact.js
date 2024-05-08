8218b29890927bdd1b5204e4e3476c000024e494
viktormashyka <99292051+viktormashyka@users.noreply.github.com>
2024-05-08 23:22:59 +0100

    fix DB_HOST

--- models/Contact.js
+++ models/Contact.js
@@ -1,22 +1,25 @@
 import { Schema, model } from "mongoose";
 
-const contactSchema = new Schema({
-  name: {
-    type: String,
-    required: [true, "Set name for contact"],
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+      unique: true,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
   },
-  email: {
-    type: String,
-    unique: true,
-  },
-  phone: {
-    type: String,
-  },
-  favorite: {
-    type: Boolean,
-    default: false,
-  },
-});
+  { versionKey: false, timestamps: true }
+);
 
 const Contact = model("contact", contactSchema);
 
